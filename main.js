12b3ca1911de5417edbafb8744ed6c5fef894bed
amirshariati <seyed.amir.shariati@gmail.com>
2024-01-31 02:04:56 +0330

    test it

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ const server = http.createServer((req, res)=>{
 server.listen(3000, ()=>{
     console.log("server is running on port 3000")
     logger.printName("Bar")
-    console.log(logger.pName)
+    console.log(logger.name)
     }
 )
