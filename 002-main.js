4408ccaba2b3380ec967a5ae789a12c7fff7e903
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-02 16:23:00 +0330

    add 002-main.js

---
+++ 002-main.js
@@ -0,0 +1,16 @@
+// const http = require("http")
+import http from 'http'
+import chalk from 'chalk';
+// const logger = require('./logger')
+import {name as logger_name, printName} from './002-logger.js'
+
+const server = http.createServer((req, res)=>{
+    console.log(req)
+})
+
+server.listen(3000, ()=>{
+        console.log(chalk.green("server is running on port 3000"))
+        printName(chalk.red("Bar"))
+        console.log(chalk.bgWhite(chalk.black(logger_name)))
+    }
+)
