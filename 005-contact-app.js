4992114e4a9b996e283851e5de48fde479f87805
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-03 00:59:09 +0330

    add yargs

--- 005-contact-app.js
+++ 005-contact-app.js
@@ -1,6 +1,4 @@
-const fs = require("fs")
-const {type} = require("os");
-const {json} = require("stream/consumers");
+const yargs = require("yargs")
 
 const data = [
     {id:1, fullName: "Amir Shariati"},
@@ -8,11 +6,4 @@ const data = [
     {id:3, fullName: "Fati Shariati"},
 ]
 
-fs.writeFileSync('005-contact.json', JSON.stringify(data))
-
-const read_data = fs.readFileSync('005-contact.json')
-const data_obj = JSON.parse(read_data.toString())
-console.log(read_data.toString())
-console.log(data_obj)
-console.log(data_obj[0]?.fullName)
-console.log(typeof data_obj)
+console.log(yargs.argv)
