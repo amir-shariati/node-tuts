045f3c2b3c4e7571a3a41254e7523e356f5ce3a6
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-02 16:45:20 +0330

    add process.on

--- 003-process.js
+++ 003-process.js
@@ -5,4 +5,9 @@ const process = require("process")
 // console.log(process.pid)
 // console.log(process.platform)
 // console.log(process.config)
-console.log(process.argv)
+// console.log(process.argv)
+process.on("start", (e)=>{
+    console.log("callback for start process event")})
+process.on("beforeExit", e=>{
+    console.log("callback for beforeExit process event")})
+console.log('start app')
