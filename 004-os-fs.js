478105393fd9ac659a71c04beaf5849450ab4001
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-02 20:53:00 +0330

    add writeFile

--- 004-os-fs.js
+++ 004-os-fs.js
@@ -3,3 +3,10 @@ const fs = require('fs')
 
 console.log(os.platform())
 console.log(os.userInfo())
+
+fs.writeFile('test-write-file.txt', 'Hello world! \n', (e)=>{
+    if (e){
+        throw e
+    }
+    console.log('write file is completed')
+})
