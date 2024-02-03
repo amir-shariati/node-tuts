9ccf3903c19ec576429f1df8f0a0524f9cab81d2
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-03 19:46:03 +0330

    check duplicate

--- 005-contact.js
+++ 005-contact.js
@@ -4,6 +4,12 @@ const addContact = (fullName, phone, email) => {
     const contacts = loadContacts()
     const isDuplicate = contacts.find(c=>c.fullName === fullName)
 
+    if (!isDuplicate){
+        console.log("contact is added")
+    }
+    else {
+        console.log("contact already existed")
+    }
 }
 
 const loadContacts = () => {
