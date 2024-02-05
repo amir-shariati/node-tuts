a0513ab2369afb4824f2c4b1f1d5ef55bec661bd
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-05 17:10:46 +0330

    add routes by if conditions

--- 007-http.js
+++ 007-http.js
@@ -4,16 +4,34 @@ const fs = require('fs')
 const server = http.createServer((req, res)=>{
     const {url, method, headers} = req
 
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>' +
-        '<header>' +
-        '<title>Hello world</title>' +
-        '</header>' +
-        '<body>' +
-        '<center><h1>Hello World!</h1></center>' +
-        '</body>' +
-        '<html/>')
-    res.end()
+    if(url === '/hello'){
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>' +
+            '<header>' +
+            '<title>Hello world</title>' +
+            '</header>' +
+            '<body>' +
+            '<center><h1>Hello World!</h1></center>' +
+            '</body>' +
+            '<html/>')
+        return res.end()
+    }
+    else if(url === '/'){
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write(
+            '<header>' +
+            '<title>Hello world</title>' +
+            '</header>'
+        )
+        res.write(
+            '<body>' +
+            '<center><h1>Home Page</h1></center>' +
+            '</body>')
+        res.write('<html/>')
+        return res.end()
+    }
+
 })
 
 server.listen(3000, ()=>{
