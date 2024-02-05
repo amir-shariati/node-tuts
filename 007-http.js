b524b536074af3bb3f3581fa6487381a8cfe6544
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-05 15:50:21 +0330

    create Server

--- 007-http.js
+++ 007-http.js
@@ -1,2 +1,9 @@
 const http = require('http')
 const fs = require('fs')
+
+const server = http.createServer((req, res)=>{
+    const url = req.url
+    const method = req.method
+    const headers = req.headers
+    console.log(`url: ${url} \n method: ${method} \n headers: ${headers} \n`)
+})
