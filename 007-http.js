aa8ca5775678ee583de2e440145ff05b35acebf9
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-05 15:55:06 +0330

    using object destructing

--- 007-http.js
+++ 007-http.js
@@ -2,10 +2,12 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res)=>{
-    const url = req.url
-    const method = req.method
-    const headers = req.headers
-    console.log(`url: ${url} \n method: ${method} \n headers: ${headers} \n`)
+    // const url = req.url
+    // const method = req.method
+    // const headers = req.headers
+    const {url, method, headers} = req
+    console.log(`url: ${url} \n method: ${method} \n`)
+    console.log('headers: ', headers , '\n')
 })
 
 server.listen(3000, ()=>{
