48549a2ffaa71d0c08ec00b3aa16d564ffa662b0
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-05 16:04:04 +0330

    return html

--- 007-http.js
+++ 007-http.js
@@ -8,6 +8,16 @@ const server = http.createServer((req, res)=>{
     const {url, method, headers} = req
     console.log(`url: ${url} \n method: ${method} \n`)
     console.log('headers: ', headers , '\n')
+
+    res.write('<html>' +
+        '<header>' +
+        '<title>Hello world</title>' +
+        '</header>' +
+        '<body>' +
+        '<center><h1>Hello World!</h1></center>' +
+        '</body>' +
+        '<html/>')
+    res.end()
 })
 
 server.listen(3000, ()=>{
