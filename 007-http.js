6abdad9325647df968c2e31b29279ea1b71a5150
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-05 17:10:11 +0330

    delete logs

--- 007-http.js
+++ 007-http.js
@@ -3,8 +3,6 @@ const fs = require('fs')
 
 const server = http.createServer((req, res)=>{
     const {url, method, headers} = req
-    console.log(`url: ${url} \n method: ${method} \n`)
-    console.log('headers: ', headers , '\n')
 
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>' +
