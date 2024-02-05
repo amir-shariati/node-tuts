4cd15b821dc83984f9e0a0221aa7d75e7d0396c4
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-05 17:09:55 +0330

    remove comment

--- 007-http.js
+++ 007-http.js
@@ -2,9 +2,6 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res)=>{
-    // const url = req.url
-    // const method = req.method
-    // const headers = req.headers
     const {url, method, headers} = req
     console.log(`url: ${url} \n method: ${method} \n`)
     console.log('headers: ', headers , '\n')
