c6ecd42e4067433aae1c6c75e30d04ef2fd7d395
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-09 18:50:11 +0330

    add root and hello path

--- 009-routes/home.js
+++ 009-routes/home.js
@@ -1,4 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/hello', (req, res) => {
+    res.send('<center><h1>Hello World</h1></center>')
+})
+
+router.get('/', (req, res) => {
+    res.send('<center><form action="admin/msg" method="post"><input type="text" name="msg"><input type="submit"></form></center>')
+})
+
 module.exports = router
