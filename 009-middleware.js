036b022e59af7a488b8d49efd9135814b8607184
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-09 23:42:24 +0330

    add path 404.html

--- 009-middleware.js
+++ 009-middleware.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -11,6 +13,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/admin', adminRoutes)
 app.use(homeRoutes)
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '009-views', '404.html'))
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
