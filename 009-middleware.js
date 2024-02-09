c485cc8592a1a92b85f65eba2563ce1746a57138
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-09 18:53:19 +0330

    use homeRoutes

--- 009-middleware.js
+++ 009-middleware.js
@@ -10,6 +10,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(adminRoutes)
+app.use(homeRoutes)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
