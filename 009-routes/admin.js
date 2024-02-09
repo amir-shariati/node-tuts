37fbfe224f43fe78fd15c5e1f6a350e0953cf78b
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-09 18:46:13 +0330

    add msg post

--- 009-routes/admin.js
+++ 009-routes/admin.js
@@ -1,7 +1,14 @@
 const fs = require('fs')
 
 const express = require('express')
-
 const router = express.Router()
+
+router.post('/msg', (req, res) => {
+    console.log(req.body)
+    fs.writeFile('007-form-data.txt', req.body.msg, err=>{
+        if (err) throw err
+        res.redirect('/')
+    })
+})
 
 module.exports = router
