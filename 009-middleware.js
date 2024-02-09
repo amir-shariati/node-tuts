9165bc620ec6ff85685253b91e463b9916a1e6fb
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-09 14:14:07 +0330

    use first middleware

--- 009-middleware.js
+++ 009-middleware.js
@@ -1,3 +1,11 @@
 const express = require('express')
 
 const app = express()
+
+app.use((req, res, next) => {
+    console.log('First middleware')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
