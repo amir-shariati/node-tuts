36483260be4157091f5988ac6c6016475268bd4b
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-09 14:16:26 +0330

    use second middleware

--- 009-middleware.js
+++ 009-middleware.js
@@ -6,6 +6,11 @@ app.use((req, res, next) => {
     console.log('First middleware')
 })
 
+app.use((req, res, next) => {
+    console.log('Second middleware')
+    res.send('Hello world')
+})
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
