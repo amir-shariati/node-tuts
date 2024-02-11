6a2e5ac87edbda7278a9be45e61bdf22b893c5bc
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-11 18:51:11 +0330

    add listener

--- 010-ejs.js
+++ 010-ejs.js
@@ -4,3 +4,7 @@ const express = require('express')
 const app = express()
 
 app.set(express.static(path.join(__dirname, '010-public')))
+
+app.listen(3000, ()=>{
+    console.log('server is running on port 3000')
+})
