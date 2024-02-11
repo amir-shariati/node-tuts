c297401ae4c21c2760104739feb8958db5d01005
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-11 18:57:33 +0330

    add / path

--- 010-ejs.js
+++ 010-ejs.js
@@ -9,6 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, '010-public')))
 
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 app.listen(3000, ()=>{
     console.log('server is running on port 3000')
 })
