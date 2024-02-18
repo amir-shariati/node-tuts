79e00cf2e6ede50f03ad018fe57af6e1a9489044
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-19 01:12:13 +0330

    edit engine and use path to set view directory

--- 011-Todo-Project/server.js
+++ 011-Todo-Project/server.js
@@ -9,8 +9,8 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 
 // EJS
-app.set('view engin', 'ejs')
-app.set('views', 'views')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 // Statics
 app.use(express.static(path.join(__dirname, '011-Todo-Project', 'public')))
