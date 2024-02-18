93ccc02d9c91117b4e7cde136d8c7c69034319e1
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-19 01:34:04 +0330

    import rootPath

--- 011-Todo-Project/server.js
+++ 011-Todo-Project/server.js
@@ -3,6 +3,8 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const rootPath = require('./utils/path')
+
 const app = express()
 
 //Middlewares
