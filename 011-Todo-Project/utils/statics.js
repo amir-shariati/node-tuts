1d8ea4f77970428fbeabbbb6a750a66956f21705
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-19 01:46:06 +0330

    export setStatics

--- 011-Todo-Project/utils/statics.js
+++ 011-Todo-Project/utils/statics.js
@@ -3,3 +3,9 @@ const path = require('path')
 const express = require('express')
 
 const rootPath = require('./path')
+
+module.exports.setStatics = (app) =>{
+    app.use(express.static(path.join(rootPath, '011-Todo-Project', 'public')))
+    app.use(express.static(path.join(rootPath, 'node_modules', 'bootstrap-v4-rtl', 'dist')))
+    app.use(express.static(path.join(rootPath, 'node_modules', 'font-awesome')))
+}
