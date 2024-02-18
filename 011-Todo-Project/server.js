0fff5706f6fca60fba0ffb9592927e9992b29511
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-19 00:35:16 +0330

    add font-awesome to static path

--- 011-Todo-Project/server.js
+++ 011-Todo-Project/server.js
@@ -15,6 +15,7 @@ app.set('views', 'views')
 // Statics
 app.use(express.static(path.join(__dirname, '011-Todo-Project', 'public')))
 app.use(express.static(path.join(__dirname, 'node_modules', 'bootstrap-v4-rtl', 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules', 'font-awesome')))
 
 //Routes
 app.listen(3000, () => {
