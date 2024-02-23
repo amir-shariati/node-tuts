7f2f51c586e1a2f781713bd2ac0924596c9f0f0a
amirshariati <seyed.amir.shariati@gmail.com>
2024-02-24 01:47:03 +0330

    change error log

--- 011-Todo-Project/controllers/admin.js
+++ 011-Todo-Project/controllers/admin.js
@@ -3,8 +3,8 @@ const Todo = require('../model/todo')
 exports.addTodo = (req, res)=>{
     if (!req.body.todo) return res.redirect('/')
     const todo = new Todo(Math.floor(Math.random()*1000), req.body.todo)
-    todo.save(err=>{
+    todo.save((err)=>{
         if (!err) res.redirect('/')
-        else console.log(err)
+        else console.log('error is: ', err)
     })
 }
